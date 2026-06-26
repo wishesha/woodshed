0b824caceab26878141421b055f8a1d9b453d09c
wishesha <wishesh.ajwalia@gmail.com>
2026-06-25 20:06:04 -0400

    Add file size information, compute time, and broader error handling
    - Show file size to user (distinquished KB vs MB)
    - Catch OSError if file is invalid, broader Exception for other audio loading errors
    - Add total compute time( in seconds)
    - Test against empty and corrupted audio files
    - Remove beat times array, not useful to user

--- src/tempo_detection.py
+++ src/tempo_detection.py
@@ -1,17 +1,42 @@
 import librosa
+import os
+import time
+import math
+from audioread.exceptions import NoBackendError
 
 audio_path = "src\Audio_Files\\"
 
 def detect_tempo(audio_path):
-    audio_path += input("Enter Filename('.wav' only): ")
+    file = input("Enter Filename('.wav' only): ")
+    audio_path += file
     try:
-        y, sr = librosa.load(audio_path) 
-    except FileNotFoundError:
-        print(f"{audio_path} is not a valid file")
+        file_size = os.path.getsize(audio_path)
+        if file_size < 1000000:
+             file_size = file_size/1000
+             byte_type = "KB"
+        else:
+            file_size = file_size/1000000
+            byte_type = "MB"
+        start_time = time.perf_counter()
+        print(f"Detected file: {file} of size {file_size} {byte_type}")
+        y, sr = librosa.load(audio_path)
+        if y.size == 0:
+            raise ValueError("Audio file contains no data.")
+    except OSError:
+        print(f"{file} is not a valid file")
         raise SystemExit
+    except Exception as e:
+            print(f"Failed to load {file} \nError: {type(e).__name__} -- {e}")
+            raise SystemExit
+    
+    
     tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
     beat_times = librosa.frames_to_time(beats, sr=sr)
-    print(f"Estimated Tempo: {tempo}\n Beat Times(in seconds): {beat_times}")
+    end_time = time.perf_counter()
+    compute_time = math.trunc((end_time - start_time) * 100) / 100
+    
+    print(f"time to complete: {compute_time} seconds")
+    print(f"Estimated Tempo: {tempo}\nBeats: {len(beat_times)}")
     return tempo
 
 detect_tempo(audio_path)
